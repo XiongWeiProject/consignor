9349050ebf2b26025cdb4f9b151c94b0bf8c5e2d
xiongwei <15111467924@163.com>
2020-05-27 10:00:46 +0800

    代码提交

--- app/src/main/java/com/zgt/consignor/MainActivity.java
+++ app/src/main/java/com/zgt/consignor/MainActivity.java
@@ -3,26 +3,70 @@ package com.zgt.consignor;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.GeolocationPermissions;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    WebView webView ;
+import com.alibaba.fastjson.JSON;
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.hdgq.locationlib.LocationOpenApi;
+import com.hdgq.locationlib.entity.ShippingNoteInfo;
+import com.hdgq.locationlib.listener.OnResultListener;
+import com.zgt.consignor.model.StartLocation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity implements AMapLocationListener {
+    WebView webView;
     private long exitTime = 0;
+    StartLocation startLocation;
+    private ValueCallback<Uri> uploadMessage;
+    private ValueCallback<Uri[]> uploadMessageAboveL;
+    private final static int FILE_CHOOSER_RESULT_CODE = 10000;
+    //声明mlocationClient对象
+    public AMapLocationClient mlocationClient;
+    //声明mLocationOption对象
+    public AMapLocationClientOption mLocationOption = null;
+    private double lat = -1;
+    private double lon = -1;
+    public String tag = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.web_view);
         initPermission();
+//        getLocation();
+
         WebSettings settings = webView.getSettings();
         settings.setDatabaseEnabled(true);
         String dir = this.getApplicationContext().getDir("database", Context.MODE_PRIVATE).getPath();
@@ -32,7 +76,41 @@ public class MainActivity extends AppCompatActivity {
         settings.setBuiltInZoomControls(false);
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        webView.getSettings().setBlockNetworkImage(false);//解决图片不显示
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            // For Android < 3.0
+            public void openFileChooser(ValueCallback<Uri> valueCallback) {
+                uploadMessage = valueCallback;
+                openImageChooserActivity();
+            }
+
+            // For Android  >= 3.0
+            public void openFileChooser(ValueCallback valueCallback, String acceptType) {
+                uploadMessage = valueCallback;
+                openImageChooserActivity();
+            }
+
+            //For Android  >= 4.1
+            public void openFileChooser(ValueCallback<Uri> valueCallback, String acceptType, String capture) {
+                uploadMessage = valueCallback;
+                openImageChooserActivity();
+            }
+
+            // For Android >= 5.0
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                uploadMessageAboveL = filePathCallback;
+                openImageChooserActivity();
+                return true;
+            }
+        });
+        webView.loadUrl("http://212.64.72.2:102/index.html");
         settings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new JSHook(), "android");
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -40,21 +118,35 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onReceivedIcon(WebView view, Bitmap icon) {
-                super.onReceivedIcon(view, icon);
+//        webView.setWebChromeClient(new WebChromeClient() {
+//            @Override
+//            public void onReceivedIcon(WebView view, Bitmap icon) {
+//                super.onReceivedIcon(view, icon);
+//
+//            }
+//
+//            @Override
+//            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+//                callback.invoke(origin, true, false);
+//                super.onGeolocationPermissionsShowPrompt(origin, callback);
+//            }
+//        });
 
-            }
+        LocationOpenApi.init(this, "com.zgt.driver", "088f5e3b5cc04abfb777bcacccc99be0dcdc0824eaf64525bf12a84667bc79030128889eacf54256a1dac44960997416"
+                , "360008", "debug", new OnResultListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(MainActivity.this, "定位上传初始化成功", Toast.LENGTH_SHORT).show();
+//
+                    }
 
-            @Override
-            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
-                callback.invoke(origin, true, false);
-                super.onGeolocationPermissionsShowPrompt(origin, callback);
-            }
-        });
-        webView.loadUrl("http://212.64.72.2:102/index.html");
+                    @Override
+                    public void onFailure(String s, String s1) {
+                        Toast.makeText(MainActivity.this, "定位上传初始化失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
+
     private void initPermission() {
         //检查权限
         String[] permissions = CheckPermissionUtils.checkPermission(this);
@@ -66,6 +158,7 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, permissions, 100);
         }
     }
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
@@ -76,10 +169,167 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
                 exitTime = System.currentTimeMillis();
             } else {
-                AppManager.getAppManager().AppExit(this,false);
+                AppManager.getAppManager().AppExit(this, false);
             }
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
+
+    public void getLocation() {
+        mlocationClient = new AMapLocationClient(this);
+//初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+//设置定位监听
+        mlocationClient.setLocationListener(this);
+//设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+//设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+//设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+// 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+// 在定位结束后，在合适的生命周期调用onDestroy()方法
+// 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+//启动定位
+        mlocationClient.startLocation();
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                lat = aMapLocation.getLatitude();//获取纬度
+                lon = aMapLocation.getLongitude();//获取经度
+                aMapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(aMapLocation.getTime());
+                df.format(date);//定位时间
+                Log.e("AmapError", "lat"
+                        + lat + "lon"
+                        + lon);
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    }
+
+    public class JSHook {
+        @JavascriptInterface
+        public void showToast(final String json) {
+            Log.d(tag, "JS发送过来的登录类型" + json);
+            startLocation = JSON.parseObject(json,StartLocation.class);
+            //做判断是那种类型调用哪个登录方法
+            if ("startLocation".equals(startLocation.getType())) {
+                ShippingNoteInfo shippingNoteInfo= new ShippingNoteInfo();
+                shippingNoteInfo.setShippingNoteNumber(startLocation.getShippingNoteNumber());
+                shippingNoteInfo.setSerialNumber(startLocation.getSerialNumber());
+                shippingNoteInfo.setStartCountrySubdivisionCode(startLocation.getStartCountrySubdivisionCode());
+                shippingNoteInfo.setEndCountrySubdivisionCode(startLocation.getEndCountrySubdivisionCode());
+                ShippingNoteInfo[] startLocation = new ShippingNoteInfo[]{shippingNoteInfo};
+                LocationOpenApi.start(MainActivity.this, startLocation, new OnResultListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(MainActivity.this, "定位上传成功", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(String s, String s1) {
+                        Toast.makeText(MainActivity.this, "定位上传失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            else if ("stopLocation".equals(startLocation.getType())) {
+                ShippingNoteInfo shippingNoteInfo= new ShippingNoteInfo();
+                shippingNoteInfo.setShippingNoteNumber(startLocation.getShippingNoteNumber());
+                shippingNoteInfo.setSerialNumber(startLocation.getSerialNumber());
+                shippingNoteInfo.setStartCountrySubdivisionCode(startLocation.getStartCountrySubdivisionCode());
+                shippingNoteInfo.setEndCountrySubdivisionCode(startLocation.getEndCountrySubdivisionCode());
+                ShippingNoteInfo[] startLocation = new ShippingNoteInfo[]{shippingNoteInfo};
+                LocationOpenApi.stop(MainActivity.this, startLocation, new OnResultListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(MainActivity.this, "定位上传已停止", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(String s, String s1) {
+                        Toast.makeText(MainActivity.this, "定位上传停止失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    }
+    private void openImageChooserActivity() {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("image/*");
+        startActivityForResult(Intent.createChooser(i, "Image Chooser"), FILE_CHOOSER_RESULT_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_CHOOSER_RESULT_CODE) {
+            if (null == uploadMessage && null == uploadMessageAboveL) return;
+            Uri result = data == null || resultCode != RESULT_OK ? null : data.getData();
+            if (uploadMessageAboveL != null) {
+                onActivityResultAboveL(requestCode, resultCode, data);
+            } else if (uploadMessage != null) {
+                uploadMessage.onReceiveValue(result);
+                uploadMessage = null;
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void onActivityResultAboveL(int requestCode, int resultCode, Intent intent) {
+        if (requestCode != FILE_CHOOSER_RESULT_CODE || uploadMessageAboveL == null)
+            return;
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK) {
+            if (intent != null) {
+                String dataString = intent.getDataString();
+                ClipData clipData = intent.getClipData();
+                if (clipData != null) {
+                    results = new Uri[clipData.getItemCount()];
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        ClipData.Item item = clipData.getItemAt(i);
+                        results[i] = item.getUri();
+                    }
+                }
+                if (dataString != null)
+                    results = new Uri[]{Uri.parse(dataString)};
+            }
+        }
+        uploadMessageAboveL.onReceiveValue(results);
+        uploadMessageAboveL = null;
+    }
+    public static Uri getImageContentUri(Context context, String filePath) {//File imageFile
+        //String filePath = imageFile.getAbsolutePath();//根据文件来获取路径
+        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[] { MediaStore.Images.Media._ID }, MediaStore.Images.Media.DATA + "=? ",
+                new String[] { filePath }, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+            Uri baseUri = Uri.parse("content://media/external/images/media");
+            return Uri.withAppendedPath(baseUri, "" + id);
+        } else {
+            if (TextUtils.isEmpty(filePath)) {//imageFile.exists()判断文件存不存在
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA, filePath);
+                return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } else {
+                return null;
+            }
+        }
+
+    }
+
 }
